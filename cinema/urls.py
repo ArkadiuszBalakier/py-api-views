596f27b4acb72fa15d7ed6e02c904acb9bb94e07
ArkadiuszBalakier <arkadiusz.balakier@gmail.com>
2026-02-06 11:30:56 +0100

    feat: add actor urls refactor: cinema_hall views without router and with mapping ing as_view method as actions

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,16 +1,27 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from cinema.views import MovieViewSet, CinemaHallViewSet, GenreListView, GenreDetailView
+from cinema.views import MovieViewSet, CinemaHallViewSet, GenreListView, GenreDetailView, ActorListView, ActorDetailView
 
 router = routers.DefaultRouter()
 router.register(r'movies', MovieViewSet)
-router.register(r'cinema_halls', CinemaHallViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
     path("genres/", GenreListView.as_view(), name="genre-list"),
     path("genres/<int:pk>/", GenreDetailView.as_view(), name="genre-detail"),
+    path("actors/", ActorListView.as_view(), name="actor-list"),
+    path("actors/<int:pk>/", ActorDetailView.as_view(), name="actor-detail"),
+    path("cinema_halls/", CinemaHallViewSet.as_view({
+        "get": "list",
+        "post": "create",
+    }), name="cinema-hall-list"),
+    path("api/cinema/cinema_halls/<int:pk>/", CinemaHallViewSet.as_view({
+        "get": "retrieve",
+        "put": "update",
+        "patch": "partial_update",
+        "delete": "destroy",
+    }))
 ]
 
 app_name = "cinema"
