e9333f72729e16dffb6abe5d61908fe7bf55a32d
ArkadiuszBalakier <arkadiusz.balakier@gmail.com>
2026-02-06 11:33:53 +0100

    refactor: removed no needed imports

--- cinema/views.py
+++ cinema/views.py
@@ -1,5 +1,4 @@
-from rest_framework.decorators import api_view
-from rest_framework.generics import GenericAPIView
+
 from rest_framework.mixins import ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, \
     DestroyModelMixin
 from rest_framework.response import Response
