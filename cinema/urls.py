12ca18320dd07cadd393997c0d09f569d785726e
ArkadiuszBalakier <arkadiusz.balakier@gmail.com>
2026-02-06 11:23:30 +0100

    feat: register path to genres and cinema halls refactor: add patch method to GenreDetailView

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from cinema.views import MovieViewSet
+from cinema.views import MovieViewSet, CinemaHallViewSet, GenreListView, GenreDetailView
 
 router = routers.DefaultRouter()
 router.register(r'movies', MovieViewSet)
+router.register(r'cinema_halls', CinemaHallViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("genres/", GenreListView.as_view(), name="genre-list"),
+    path("genres/<int:pk>/", GenreDetailView.as_view(), name="genre-detail"),
 ]
 
 app_name = "cinema"
--- cinema/views.py
+++ cinema/views.py
@@ -44,6 +44,13 @@ class GenreDetailView(APIView):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+    def patch(self, request, pk):
+        genre = get_object_or_404(Genre, pk=pk)
+        serializer = GenreSerializer(instance=genre, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
     def delete(self, request, pk):
         genre = get_object_or_404(Genre, pk=pk)
         genre.delete()
